fe49a4c9059235fe35244db5e5e03f7c646ffa62
Davide Zoccarato <dzoccarato@live.it>
2019-12-23 12:39:36 +0100

    add JWT Bearer Autentication on swagger

--- ASP.NET-Core/RegisterSwagger.cs
+++ ASP.NET-Core/RegisterSwagger.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
 using System;
+using System.Collections.Generic;
 
 namespace ASPNETCore
 {
@@ -19,7 +21,7 @@ namespace ASPNETCore
 #if DEBUG
             // Register the Swagger generator, defining 1 or more Swagger documents
             self.AddSwaggerGen(c =>
-           {
+            {
                 c.SwaggerDoc("v1", new OpenApiInfo
                 {
                     Title = "ASPNETCore API",
@@ -32,12 +34,44 @@ namespace ASPNETCore
                         Url = new Uri("https://www.davidezoccarato.cloud/")
                     },
                 });
+
 #pragma warning disable CS0618 // Type or member is obsolete
                 c.DescribeAllEnumsAsStrings();
 #pragma warning restore CS0618 // Type or member is obsolete
+
                 c.IncludeXmlComments(string.Format(@"{0}\ASP.NET-Core.xml", System.AppDomain.CurrentDomain.BaseDirectory));
                 c.IgnoreObsoleteProperties();
-           });
+
+                // ref: https://stackoverflow.com/questions/56234504/migrating-to-swashbuckle-aspnetcore-version-5
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                    }
+                });
+
+            });
 #endif
         }
 
