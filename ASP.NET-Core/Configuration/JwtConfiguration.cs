129ddb481ca3f746ae7266692d9899e1f894c17b
Davide Zoccarato <dzoccarato@live.it>
2020-01-25 15:12:52 +0100

    Class that binds appsetting.json file

---
+++ ASP.NET-Core/Configuration/JwtConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace ASPNETCore.Configuration
+{
+    /// <summary>
+    /// Wrapper for Jwt section from configuration file.
+    /// Used as <seealso cref="IOptionsMonitor&lt;JwtConfiguration&gt;"/>
+    /// </summary>
+    public class JwtConfiguration : IAppSettingConfiguration
+    {
+        /// <summary>
+        /// Jwt Key. Used to create <seealso cref="SymmetricSecurityKey"/> 
+        /// </summary>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// Jwt Issuer. Used to create <seealso cref="JwtSecurityToken"/> 
+        /// </summary>
+        public string Issuer { get; set; }
+    }
+}
