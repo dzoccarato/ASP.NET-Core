12452979fba0fbfe4a607042865f0b04a9d03459
Davide Zoccarato <dzoccarato@live.it>
2020-01-25 15:13:04 +0100

    registrer IOptionMonitorObject

--- ASP.NET-Core/Startup.cs
+++ ASP.NET-Core/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using System.Security.Principal;
 
 namespace ASPNETCore
@@ -16,18 +18,18 @@ namespace ASPNETCore
     public class Startup
     {
         /// <summary>
+        /// IConfiguration
+        /// </summary>
+        private readonly IConfiguration _configuration;
+
+        /// <summary>
         /// ctor
         /// </summary>
         /// <param name="configuration"></param>
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
-
-        /// <summary>
-        /// IConfiguration
-        /// </summary>
-        public IConfiguration Configuration { get; }
 
         /// <summary>
         /// This method gets called by the runtime. Use this method to add services to the container. 
@@ -41,9 +43,13 @@ namespace ASPNETCore
             services.AddTransient<IPrincipal>(provider => provider.GetService<IHttpContextAccessor>()?.HttpContext?.User);
 
             //
+            services.AddSingleton(typeof(IOptionsMonitor<>), typeof(OptionsMonitor<>));
+
+            //
             services.ConfigureSwaggerService();
-            services.ConfigureJWTService(Configuration);
-            services.ConfigureAppServices(Configuration);
+            services.ConfigureJWTService(_configuration);
+            services.ConfigureAppServices(_configuration);
+
         }
 
         /// <summary>
