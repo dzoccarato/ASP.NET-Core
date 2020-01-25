7568f4f5a6f90823f17c37b3d69d51bc5b16cc33
Davide Zoccarato <dzoccarato@live.it>
2020-01-25 15:14:53 +0100

    use IConfiguration and IOption monitor to triger a mesage when JWT section changed

--- ASP.NET-Core/RegisterJWT.cs
+++ ASP.NET-Core/RegisterJWT.cs
@@ -1,17 +1,23 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using ASPNETCore.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
 namespace ASPNETCore
 {
+    using ASPNETCore.MethodExtensions;
+
     /// <summary>
     /// used to add JWT to project
     /// </summary>
     public static class RegisterJWT
     {
+        private static JwtConfiguration original;
         /// <summary>
         /// Configure JWT. use in <seealso cref="Startup.ConfigureServices(IServiceCollection)"/>
         /// </summary>
@@ -20,6 +26,8 @@ namespace ASPNETCore
         public static void ConfigureJWTService(this IServiceCollection self, IConfiguration configuration)
         {
             self.Configure<JwtConfiguration>(configuration.GetSection(ConfigurationKeys.Jwt));
+            original = configuration.Get<JwtConfiguration>(ConfigurationKeys.Jwt);
+
             System.IdentityModel.Tokens.Jwt.JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
 
             self.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -31,9 +39,9 @@ namespace ASPNETCore
                         ValidateAudience = true,
                         ValidateLifetime = true,
                         ValidateIssuerSigningKey = true,
-                        ValidIssuer = configuration["Jwt:Issuer"],
-                        ValidAudience = configuration["Jwt:Issuer"],
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]))
+                        ValidIssuer = original.Issuer ?? string.Empty,
+                        ValidAudience = original.Issuer ?? string.Empty,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(original.Key ?? string.Empty))
                     };
                 });
         }
@@ -45,6 +53,23 @@ namespace ASPNETCore
         public static void ConfigureJWT(this IApplicationBuilder self)
         {
             self.UseAuthentication();
+
+            using (var serviceScope = ServiceActivator.GetScope())
+            {
+                ILoggerFactory loggerFactory = serviceScope.ServiceProvider.GetService<ILoggerFactory>();
+                IOptionsMonitor<JwtConfiguration> option = (IOptionsMonitor<JwtConfiguration>)serviceScope.ServiceProvider.GetService(typeof(IOptionsMonitor<JwtConfiguration>));
+                
+
+                ILogger logger = loggerFactory.CreateLogger(typeof(RegisterJWT));
+
+                option.OnChange((o, s) =>
+                {
+                    if(original?.Issuer != option?.CurrentValue.Issuer || original?.Key != option?.CurrentValue.Key)
+                        logger.LogWarning($"JwtConfiguration changed. Restart application to apply changes or discard them");
+                    else
+                        logger.LogInformation($"JwtConfiguration reset to original.");
+                });
+            }
         }
     }
 }
