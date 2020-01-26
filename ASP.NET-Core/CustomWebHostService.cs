c8f2f153da315b15921a7ff106877046a7a3a227
Davide Zoccarato <dzoccarato@live.it>
2020-01-26 16:27:52 +0100

    add method extension and class inheritance to use custom web host service

---
+++ ASP.NET-Core/CustomWebHostService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ASPNETCore
+{
+    internal class CustomWebHostService : WebHostService
+    {
+        private ILogger _logger;
+
+        public CustomWebHostService(IWebHost host) : base(host)
+        {
+            _logger = host.Services
+                .GetRequiredService<ILogger<CustomWebHostService>>();
+        }
+
+        protected override void OnStarting(string[] args)
+        {
+            _logger.LogInformation($"Starting");
+            base.OnStarting(args);
+        }
+
+        protected override void OnStarted()
+        {
+            _logger.LogInformation($"Started");
+            base.OnStarted();
+        }
+
+        protected override void OnStopping()
+        {
+            _logger.LogInformation($"Stopping");
+            base.OnStopping();
+        }
+
+        protected override void OnStopped()
+        {
+            _logger.LogInformation($"Stopped");
+            base.OnStopped();
+        }
+    }
+}
---
+++ ASP.NET-Core/MethodExtensions/WebHostServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System.ServiceProcess;
+
+namespace ASPNETCore.MethodExtensions
+{
+    /// <summary>
+    /// WebHostServiceExtensions
+    /// </summary>
+    public static class WebHostServiceExtensions
+    {
+        /// <summary>
+        /// RunAsCustomService
+        /// </summary>
+        /// <param name="host"></param>
+        public static void RunAsCustomService(this IWebHost host)
+        {
+            var webHostService = new CustomWebHostService(host);
+            ServiceBase.Run(webHostService);
+        }
+    }
+}
