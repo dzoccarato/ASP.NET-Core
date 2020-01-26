ac1346d1d56900b56fe45edba7ee5682a9cd023b
Davide Zoccarato <dzoccarato@live.it>
2020-01-26 16:27:27 +0100

    add cnfiguration utils to build configuration without DI

---
+++ ASP.NET-Core/Configuration/ConfigurationUtils.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ASPNETCore.Configuration
+{
+    /// <summary>
+    /// ConfigurationUtils
+    /// </summary>
+    public static class ConfigurationUtils
+    {
+        /// <summary>
+        /// BuildConfigurations
+        /// </summary>
+        /// <returns></returns>
+        public static IConfigurationRoot BuildConfiguration() =>
+            new ConfigurationBuilder()
+                .SetBasePath(System.IO.Path.GetDirectoryName(new System.Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase).LocalPath))
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile("appsettings.development.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("appsettings.production.json", optional: true, reloadOnChange: true)
+                .Build();
+
+    }
+}
